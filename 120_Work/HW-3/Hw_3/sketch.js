ce75623157e0312fe74a5f4007f7e3c24001a269
HagenNathaniel <42828220+HagenNathaniel@users.noreply.github.com>
2018-09-11 18:31:46 -0600

    update

---
+++ 120_Work/HW-3/Hw_3/sketch.js
@@ -0,0 +1,23 @@
+// Declare the Setup function
+
+function setup() {
+
+// create a canvas 1280 px wide, by 720 px high
+
+    createCanvas( 1280, 720);
+
+// Color the background 'magenta'
+
+    background( 'magenta' );
+}
+// Declare the draw function
+
+function draw()  {
+
+// tell p5 to print the text string "Hello World!"
+
+//this is going to be positioned at ( x 610, 575 ).
+
+text( "Hello World!", 610, 365 );
+
+}
