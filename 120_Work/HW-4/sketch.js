b4c62b21170ab7cdcd99c79b4377376b2b6ba452
Nathaniel Hagen <42828220+HagenNathaniel@users.noreply.github.com>
2018-09-17 12:42:55 -0600

    Add files via upload

--- 120_Work/HW-4/sketch.js
+++ 120_Work/HW-4/sketch.js
@@ -4,4 +4,70 @@ function setup() {
 
 // create a canvas 1280 px wide, by 720 px high
 
+  Createcanvas( 1280, 720);
+
+
+//color background 'grey'
+
+background( 'grey');
+
+// create initial 'body' of monster. using an elongated oval, or ellipse
+
+function draw() {
+
+  ellipse( 10, 10, 30, 80);
+
+  // creates an ellipse, that's taller, than it is wide.
+ }
+
+// create a series of ellipses for left arm
+
+function draw(){
+
+  ellipse( 20, 20, 20, 51);
+
+}
+
+function draw() {
+
+  ellipse( 25, 16, 20 51);
+
+}
+
+// create series of ellipses for right arm.
+
+function draw(){
+
+  ellipse( 60, 60, 20, 51);
+
+}
+
+function draw(){
+
+  ellipse( 65, 65, 20, 51);
+
+}
+
+// create ellipses for left leg.
+
+function draw(){
+
+  ellipse( 75, 75, 20, 58);
+
+}
+
+function draw(){
+
+  ellipse( 80, 80, 20, 58);
+
+}
+
+// create ellipses for right leg.
+
+function draw(){
+
+  ellipse( 90, 90, 20, 58);
+
+}
+
 }
