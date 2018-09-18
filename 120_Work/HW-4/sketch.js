b4adc2de8784b0c16230c086d58dd87e06fc9d1f
Nathaniel Hagen <42828220+HagenNathaniel@users.noreply.github.com>
2018-09-17 21:07:38 -0600

    Add files via upload

--- 120_Work/HW-4/sketch.js
+++ 120_Work/HW-4/sketch.js
@@ -14,7 +14,7 @@ background( 'grey');
 // create initial 'body' of monster. using an elongated oval, or ellipse
 
 function draw() {
-
+  fill('dark blue');
   ellipse( 10, 10, 30, 80);
 
   // creates an ellipse, that's taller, than it is wide.
@@ -23,13 +23,13 @@ function draw() {
 // create a series of ellipses for left arm
 
 function draw(){
-
+  fill( 'Light Brown');
   ellipse( 20, 20, 20, 51);
 
 }
 
 function draw() {
-
+  fill( 'Light brown');
   ellipse( 25, 16, 20 51);
 
 }
@@ -37,13 +37,13 @@ function draw() {
 // create series of ellipses for right arm.
 
 function draw(){
-
+  fill( 'Light Brown');
   ellipse( 60, 60, 20, 51);
 
 }
 
 function draw(){
-
+  fill( 'Light Brown');
   ellipse( 65, 65, 20, 51);
 
 }
@@ -51,13 +51,13 @@ function draw(){
 // create ellipses for left leg.
 
 function draw(){
-
+  fill( ; 'Light Brown');
   ellipse( 75, 75, 20, 58);
 
 }
 
 function draw(){
-
+  fill( 'Light Brown');
   ellipse( 80, 80, 20, 58);
 
 }
@@ -65,8 +65,15 @@ function draw(){
 // create ellipses for right leg.
 
 function draw(){
-
+  fill( 'Light Brown');
   ellipse( 90, 90, 20, 58);
+
+}
+//Create circle for head. (almost forgot!!)
+
+function draw(){
+  fill( 'Red');
+  ellipse( 15, 15, 60, 60 );
 
 }
 
