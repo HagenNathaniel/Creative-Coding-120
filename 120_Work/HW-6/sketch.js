607efd224dfaf4f780182748039832ba46023ba3
Nathaniel Hagen <42828220+HagenNathaniel@users.noreply.github.com>
2018-10-04 16:15:10 -0600

    Add files via upload

---
+++ 120_Work/HW-6/sketch.js
@@ -0,0 +1,76 @@
+
+//creat a canvas, that's 1920x1080 pxl's in size, and blue.
+
+function setup(){
+
+  createCanvas ( 600, 600);
+
+  background ('blue');
+
+  }
+
+// setup global variables for addition, subtraction, multiplication, division, and modulo functions.
+
+let var1 = 25;  // addition variable 1
+
+let var2 = 25;  // addition var 2
+
+let var3 = 100;  //subtraction var 1
+
+let var4 = 50;  //sub var 2
+
+let var5 = 10;  // multiplication var 1
+
+let var6 = 5;  // multiplication var 2
+
+let var7 = 600;  //division var 1
+
+let var8 = 2;  //division var 2
+
+// mathematical function for addition
+
+ var9 = var1 + var2;
+
+ var9;
+
+ // mathematical function for subraction
+
+var10 = var3 - var4;
+
+var10;
+
+// mathematical function for multiplication.
+
+ var11 = var5 * var6;
+
+ var11;
+
+ // mathematical function for division
+
+var12 = var7 / var8;
+
+ var12;
+
+
+//function draw(){
+
+
+  var13 = ceil( Math.floor(Math.random() * 31 + 1)); //random number generator for modulos
+
+  // store results as variables for drawing part of the program
+
+  var13;
+}
+// use random function, coupled with 'floor' to generate a number between 1 and 30 for second modulos number
+
+
+  var14 = ceil( Math.floor(Math.random() * 61 + 15)); // random number generator 2 for second modulos number
+
+  var14;
+
+  var15 = var9 % var10;
+
+  var15;
+
+
+ // store results as variables for drawing part of the program
