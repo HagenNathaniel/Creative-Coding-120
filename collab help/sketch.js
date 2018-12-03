d34b57b6dda9c64e0f87586ad55f4d9dc6326b04
Nathaniel Hagen <42828220+HagenNathaniel@users.noreply.github.com>
2018-12-03 10:49:22 -0700

    Add files via upload

---
+++ collab help/sketch.js
@@ -0,0 +1,87 @@
+/*jshint esversion: 6 */
+
+// Declare the Setup function
+
+//setting default gamescreen as 0;
+
+let quizScreen = 0;
+
+// create variable for button object.
+
+var button1;
+var button2;
+function setup() {
+
+// create a canvas 1280 px wide, by 720 px high, with a red background
+
+createCanvas (1280, 720);
+
+background('red');
+
+button = createButton('Game Start');
+button.position (height/2, width/2);
+button.mousePressed(gameStart);
+
+button2 = createButton('Quit Game');
+button.position (height/2 - 15, width/2);
+button.mousePressed(gameEndScreen);
+}
+function draw(){
+
+// displays the contents of the current selected screen
+
+if (quizScreen == 0) {
+
+  initScreen();
+
+} else if (quizScreen == 1){
+
+  gameScreen();
+
+} else if (quizScreen== 2){
+
+  gameEndScreen();
+
+}
+}
+
+// Contents of game screeens
+
+function initScreen() {
+// code for "Game Start" screen
+
+background('blue');
+textAlign(CENTER);
+text("Click to Start Game", height/2, width/2);
+
+}
+
+function gameScreen(){
+
+  //code for game screen/s
+}
+
+function gameEndScreen(){
+
+  // code for game end screen, user requested.
+}
+
+function mousePressed(){
+
+  if (gameScreen==0){
+  gameStart();
+}
+}
+
+//setting necessary paraeters to start gameEndScreen
+
+function gameStart(){
+
+  quizScreen=1;{
+}
+
+
+// user inputs
+
+
+}
