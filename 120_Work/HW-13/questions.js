f09ea1b0a59668b3d9abdfc7b45a749124665b46
Nathaniel Hagen <42828220+HagenNathaniel@users.noreply.github.com>
2018-12-08 17:21:23 -0700

    Add files via upload

--- 120_Work/HW-13/questions.js
+++ 120_Work/HW-13/questions.js
@@ -1,19 +1,91 @@
 let questions = [];
 
 questions[0] = {
-  question: "what is the day?",
+  question: "What is the developer of World of Warcraft?",
   answers: [
-    {text: "Thursday", isCorrect: true},
-    {text: "Sunday", isCorrect: false},
-    {text: "Friday", isCorrect: false}
+    {text: "Ubisoft", isCorrect: false},
+    {text: "Electronic Arts", isCorrect: false},
+    {text: "Blizzard Entertainment", isCorrect: true}
   ]
 };
 
 questions[1] = {
-  question: "what is the month",
+  question: "What is Call of Duty's Genre?",
   answers: [
-    {text: "May", isCorrect: false},
-    {text: "June", isCorrect: false},
-    {text: "July", isCorrect: true}
+    {text: "FPS", isCorrect: true},
+    {text: "Role Playing/Stategy", isCorrect: false},
+    {text: "MMO", isCorrect: false}
+  ]
+};
+
+questions[2] = {
+  question: "What is the Name of the ship the Doctor travels in?",
+  answers: [
+    {text: "Enterprise", isCorrect: false},
+    {text: "TARDIS", isCorrect: true},
+    {text: "MMO", isCorrect: false}
+  ]
+};
+
+questions[3] = {
+  question: "Who Developed the Playstation 1?",
+  answers: [
+    {text: "Microsoft", isCorrect: false},
+    {text: "Guerrilla Games", isCorrect: false},
+    {text: "Sony", isCorrect: true}
+  ]
+};
+
+questions[4] = {
+  question: "What is the main Antagonist in Spyro The Dragon?",
+  answers: [
+    {text: "Gnasty Gnorc", isCorrect: true},
+    {text: "Deathwing", isCorrect: false},
+    {text: "Otto Octavius", isCorrect: false}
+  ]
+};
+
+questions[5] = {
+  question: "Who is the Protagonist in the syphon Filter Series?",
+  answers: [
+    {text: "James Hewlett", isCorrect: false},
+    {text: "Nathan Drake", isCorrect: false},
+    {text: "Gabe Logan", isCorrect: true}
+  ]
+};
+
+questions[6] = {
+  question: "What is the name of the Device used to travel through wormholes?",
+  answers: [
+    {text: "Prometheus", isCorrect: false},
+    {text: "Stargate", isCorrect: true},
+    {text: "Wraith", isCorrect: false}
+  ]
+};
+
+questions[7] = {
+  question: "",
+  answers: [
+    {text: "James Hewlett", isCorrect: false},
+    {text: "Nathan Drake", isCorrect: false},
+    {text: "Gabe Logan", isCorrect: true}
+  ]
+};
+
+questions[8] = {
+  question: "Who is the Antagonist in the syphon Filter Series?",
+  answers: [
+    {text: "James Hewlett", isCorrect: false},
+    {text: "Nathan Drake", isCorrect: false},
+    {text: "Gabe Logan", isCorrect: true}
+  ]
+};
+
+questions[9] = {
+  question: "Who is the Antagonist in the syphon Filter Series?",
+  answers: [
+    {text: "James Hewlett", isCorrect: false},
+    {text: "Nathan Drake", isCorrect: false},
+    {text: "Gabe Logan", isCorrect: true}
   ]
 };
